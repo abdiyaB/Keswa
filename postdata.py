b95be7c2df332900dceed30c617b9772bc8aa33b
abdiya <127060798+abdiyaB@users.noreply.github.com>
2024-04-27 19:01:10 +0300

    Add files via upload

---
+++ postdata.py
@@ -0,0 +1,17 @@
+posts = [
+    {
+      "name": "John Doe",
+      "email": "john.doe@example.com",
+      "amount": 50,
+      "frequency": "One Time",
+      "payment": "Debit or Credit card"
+    },
+    {
+      "name": "Jane Doe",
+      "email": "jane.doe@example.com",
+      "amount": 100,
+      "frequency": "Monthly",
+      "payment": "Apple Pay"
+    }
+  ]
+
