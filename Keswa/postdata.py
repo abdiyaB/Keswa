57488b24f08762c5560db3efa4fb1e513f224b17
widaljehani <165739412+widaljehani@users.noreply.github.com>
2024-04-27 23:49:41 +0300

    Update postdata.py

--- Keswa/postdata.py
+++ Keswa/postdata.py
@@ -1,23 +1,23 @@
 posts = [
     {
+        'id': 1,
         'name': 'Wed Aljehani',
         'email': 'wed_aljehani@gmail.com',
         'phone_number': '0555522112',
-        'cv': ("http://www.wid/cv.com"),
         'comment': 'No Comment'
     },
     {
+        'id': 2,
         'name': 'Abdiya Bargah',
         'email': 'Abdiya_Bargah@yahoo.com',
         'phone_number': '0551122333',
-        'cv': ("http://www.abdiya/cv.com"),
         'comment': 'No Comment'
     },
     {
+        'id': 3,
         'name': 'Sulafah kaki',
         'email': 'Sulafah_kaki@hotmail.com',
         'phone_number': '0505598766',
-        'cv': ("http://www.sulafah/cv.com"),
         'comment': 'No Comment'
     }
 ]
