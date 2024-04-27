c66db1b560ee3af6d8b90e7c2c1cb5a80851db4f
abdiya <127060798+abdiyaB@users.noreply.github.com>
2024-04-27 20:33:40 +0300

    Add files via upload

---
+++ Keswa/flaskblog.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template ,request
+from postdata import posts
+from flask_mail import Mail,Message
+
+app = Flask(__name__)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+app.config['MAIL_USERNAME'] = 'Keswa@charity.org'
+app.config['MAIL_PASSWORD'] = 'keswa@charity'
+
+mail = Mail(app)
+
+
+@app.route("/clothes")
+def homePage():
+   return render_template('clothes.html')
+
+
+@app.route("/money")
+def about():
+    return render_template('money.html')
+
+
+@app.route("/index" , methods = ['GET','POST'])
+def contactUs():
+    if request.method == 'POST' : 
+        msg = Message("Hey" , sender='noreply@demo.com', recipients=['g4project.se421@gmail.com'])
+        msg.body = " hey how are you"
+        mail.send(msg)
+        return "sent email"
+    return render_template('index.html')
+
+#debug tool
+if __name__ == '__main__':
+	app.run( debug=True )
