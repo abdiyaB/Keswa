728fdb0188298210c4bbefe2fa65fb0d495577ca
abdiya <127060798+abdiyaB@users.noreply.github.com>
2024-04-28 18:12:53 +0300

    Add files via upload

---
+++ Keswa/postdata.py
@@ -0,0 +1,23 @@
+posts = [
+    {
+        'id': 1,
+        'name': 'Wed Aljehani',
+        'email': 'wed_aljehani@gmail.com',
+        'phone_number': '0555522112',
+        'comment': 'No Comment'
+    },
+    {
+        'id': 2,
+        'name': 'Abdiya Bargah',
+        'email': 'Abdiya_Bargah@yahoo.com',
+        'phone_number': '0551122333',
+        'comment': 'No Comment'
+    },
+    {
+        'id': 3,
+        'name': 'Sulafah kaki',
+        'email': 'Sulafah_kaki@hotmail.com',
+        'phone_number': '0505598766',
+        'comment': 'No Comment'
+    }
+]
